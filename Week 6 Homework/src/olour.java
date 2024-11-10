24af117c64f3f9f005eb154aefccff18139c1a6c
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-10 18:02:21 +0000

    my first push

---
+++ Week 6 Homework/src/olour.java
@@ -0,0 +1,10 @@
+public class olour {
+    public static void main(String[] args) {
+
+        String name = "amin";
+        int age = 20;
+        String color = "Black";
+
+        System.out.println("My name is "+name+" and I am "+age+ "year old. "+"My Favourite color is "+ color);
+    }
+}
