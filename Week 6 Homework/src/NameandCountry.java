f4a173c623cebf3999334a667a44ac903f8d4925
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-10 17:06:50 +0000

    my first push

---
+++ Week 6 Homework/src/NameandCountry.java
@@ -0,0 +1,9 @@
+public class NameandCountry {
+
+    public static void main(String[] args) {
+
+        String name = "Kenny";
+        String Country = "Paris";
+        System.out.println("My name is " +name+ " and I lives in " + Country );
+    }
+}
