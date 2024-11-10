9810361d69e3f5ac162b18a538a7f5623b37b3a3
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-10 17:48:06 +0000

    my first push

---
+++ Week 6 Homework/src/Print3.java
@@ -0,0 +1,13 @@
+public class Print3 {
+    public static void main(String[] args) {
+
+        System.out.println("               1            ");
+        System.out.println("             1    1          ");
+        System.out.println("           1    2   1         ");
+        System.out.println("         1   3    3   1        ");
+        System.out.println("       1   4   6    4   1       ");
+        System.out.println("     1   5   10  10   5   1    ");
+        System.out.println("   1   6   15  20  15   6   1  ");
+        System.out.println( "1   7   21  35  35   21   7   1");
+    }
+}
