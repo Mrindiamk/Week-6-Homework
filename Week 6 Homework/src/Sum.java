250b8e04e29d06fd5c5f5840888466de4539e8b3
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-13 15:16:48 +0000

    my first push

---
+++ Week 6 Homework/src/Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    public static void main(String[] args) {
+
+        int num1= 45;
+        int num2= 15;
+        System.out.println(num1+num2);
+        System.out.println(num1*num2);
+        System.out.println(num1-num2);
+
+    }
+}
