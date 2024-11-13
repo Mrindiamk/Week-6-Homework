1af453e0006c43027de20ee2dda136e9527e7be4
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-13 15:46:56 +0000

    my last push

---
+++ Week 6 Homework/src/PrintMyFullName.java
@@ -0,0 +1,9 @@
+public class PrintMyFullName {
+    public static void main(String[] args) {
+
+        String Name="Rekha";
+        String Surname= "Panchal";
+
+        System.out.println("Hello, I am "+Name +" "+Surname +" !");
+    }
+}
