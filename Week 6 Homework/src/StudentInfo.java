631d7f25374bfbcb99f064ff64e80292adfa4bda
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-13 15:37:39 +0000

    my second push

---
+++ Week 6 Homework/src/StudentInfo.java
@@ -0,0 +1,14 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+
+
+        String Name = "Rekha";
+        int Age = 22;
+        char Grade = 'A';
+        double average_score = 80;
+
+        System.out.println("My name is "+Name+" I am "+Age+" Year old And I got grade "+Grade+" in my class And my average score in my school is "+ average_score);
+
+
+    }
+}
